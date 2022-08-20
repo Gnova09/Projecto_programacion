956b7e37b1e57ddb96542b8478761d8b2c02b3f3
Georges Bueno <georges.bueno@hotmail.com>
2022-08-20 02:05:00 -0400

    some changes
    Metodo delete y se configuro el boton de exit

--- JavaApplication1/src/ConexionSQL.java
+++ JavaApplication1/src/ConexionSQL.java
@@ -1,14 +1,19 @@
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexionSQL {
     
-         Connection SqlCn;
-	private final String Nombre_db ="login_database";
+        private Connection SqlCn;
+	private final String Nombre_db ="escuela"; //Cambiar nombre de la base de dasto
 	private final String usuario ="root";
 	private final String password ="1009";
 	private final String url ="jdbc:mysql://localhost:3306/";
         private final String driver = "com.mysql.cj.jdbc.Driver";
+        
+        private Statement stm =null;
+	private ResultSet Rs = null;
         
         public Connection getConnetion() {
 		try {
@@ -27,5 +32,27 @@ public class ConexionSQL {
 			System.out.println("Ocurre una SQLException: "+e.getMessage());
 		}
 		return SqlCn;
+	}
+        ///////METODO PARA ELIMINAR BY ID///////
+        public boolean DeleteUser(String Matricula) {
+		boolean valid = false;
+		
+		try {
+			SqlCn = getConnetion();
+			stm = SqlCn.createStatement();
+			stm.executeUpdate("DELETE FROM estudiantes WHERE Matricula = '"+Matricula+"'");
+			System.out.println("Estudiante eliminado");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("ERROR: "+e);
+		}finally {
+			valid=true;
+                    try {
+                        SqlCn.close();
+                    } catch (SQLException ex) {
+                        Logger.getLogger(ConexionSQL.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+		}
+		return valid;
 	}
 }
